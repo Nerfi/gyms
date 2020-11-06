710dbf1082c4f9c18b9b79995b5e18f3d56799ca
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-11-06 14:06:38 +0100

    not sure why img appers so far away

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import ReactMapGL, {Marker,Popup} from 'react-map-gl';
+import {gyms} from '../src/data/gyms.json';
 import './App.css';
+
+
 
 function App() {
   //setting the viewpots state, the default
   const [viewport, setViewport] = useState({
-    latitude: 37.9870400,
-    longitude: -1.1300400,
+    latitude: 13.75398,
+    longitude: 100.50144,
     width: '100vw',
     height: '100vh',
     zoom: 10
@@ -15,13 +18,6 @@ function App() {
   //taking the selected gym from the user
   //at first the user did not choose any gym yet
   const [selectedGym, setSelected] = useState(null);
-
-  //fake data in order to test the app fast
-  const fakeData = {
-    name: 'Some random name',
-    street: 'ever green terraze',
-    location: 'Thailand'
-  }
 
   return (
     <div>
@@ -33,26 +29,34 @@ function App() {
       }}
       mapStyle="mapbox://styles/nerfi/ckh52m6tn02fe1an3lv1yruqh"
       >
-    {/*I will need to create the API firsst in order to render a marker where the user
-    click in
-  */}
-      <Marker latitude={viewport.latitude} longitude={viewport.longitude}>
-      <button onClick={e =>{
-        e.preventDefault();
-        setSelected(e.target)
-      }}>
-        <img src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=625&q=80" alt="unsplash" style={{width: '30px'}}/>
-      </button>
 
 
-      </Marker>
-      {selectedGym ?(
-          <Popup latitude={viewport.latitude} longitude={viewport.longitude} onClose={() =>  {
+      {gyms.map(gym => (
+
+        <Marker key={gym.name}
+         latitude={gym.geometry.coordinates[0]}
+          longitude={gym.geometry.coordinates[1]}
+        >
+        <button onClick={e => {
+          e.preventDefault();
+          setSelected(gym);
+        }}>
+          <img src="https://images.unsplash.com/photo-1521800641212-77d98bb90d21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80" alt="unsplash" style={{width: '80px'}}/>
+        </button>
+
+
+        </Marker>
+        ))}
+
+      {selectedGym ? (
+
+          <Popup latitude={selectedGym.geometry.coordinates[0]} longitude={selectedGym.geometry.coordinates[1]} onClose={() =>  {
             setSelected(null);
-          }}>
+          }}
+        >
         <div>
-        <h2>{fakeData.name}</h2>
-        <p>{fakeData.street}</p>
+        <h2>{selectedGym.name}</h2>
+        <p>{selectedGym.country}</p>
 
         </div>
           </Popup>
@@ -63,3 +67,49 @@ function App() {
 }
 
 export default App;
+/*
+
+ <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_TOKEN}
+        mapStyle="mapbox://styles/nerfi/ckh52m6tn02fe1an3lv1yruqh"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      >
+        {gyms.map(gym => (
+          <Marker
+            key={gym.name}
+            latitude={gym.geometry.coordinates[1]}
+            longitude={gym.geometry.coordinates[0]}
+          >
+            <button
+              className="marker-btn"
+              onClick={e => {
+                e.preventDefault();
+                setSelectedPark(park);
+              }}
+            >
+              <img src="/skateboarding.svg" alt="Skate Park Icon" />
+            </button>
+          </Marker>
+        ))}
+
+        {selectedPark ? (
+          <Popup
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedPark(null);
+            }}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <p>{selectedPark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
+    </div>
+    */
